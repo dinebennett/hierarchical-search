ae1de71faa8011af2fa84b8d91b006038074456e
dinebennett <poekie123>
2015-07-03 10:47:13 +0200

    Added multiple category selection support.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,8 +4,11 @@ export default Ember.Controller.extend({
   levelOneDisplayed: false,
   //levelOneSelection - set in router
   levelTwoDisplayed: false,
-  levelTwoSelection: null,
+  levelTwoSelection: [],
   productsToDisplay: null,
+  actualMinMaxPrice: function () {
+    return getMinMaxPrice(this.get('availableItemsNoPriceLimits'));
+  }.property('availableItemsNoPriceLimits'),
   priceRange: function () {
     var range = {
       'min': [this.get('actualMinMaxPrice')[0]],
@@ -13,16 +16,6 @@ export default Ember.Controller.extend({
     };
     return range;
   }.property('actualMinMaxPrice'),
-  actualMinMaxPrice: function () {
-    return getMinMaxPrice(this.get('availableItemsNoPriceLimits'));
-  }.property('availableItemsNoPriceLimits'),
-  noItemsFound: function () {
-    if (this.get('productsToDisplay') === null) {
-      return false;
-    } else {
-      return (this.get('productsToDisplay').length === 0);
-    }
-  }.property('productsToDisplay'),
   levelOneItems: function () {
     //TODO: Mark which one is selected
     var modelItems = this.get('model');
@@ -41,12 +34,27 @@ export default Ember.Controller.extend({
       }
     });
   }.property('levelOneSelection'),
+  levelTwoSelectionString: function() {
+    if (this.get('levelTwoSelection').length > 1) {
+      return "Multiple...";
+    } else if (this.get('levelTwoSelection').length === 1) {
+      return (this.get('levelTwoSelection'))[0].name;
+    }
+    return "All";
+  }.property('levelTwoSelection'),
   availableItems: function () {
     return getAvailableItems(this.get('levelTwoSelection'), this.get('levelTwoItems'), this.get('model'), this.get('displayedMinPrice'), this.get('displayedMaxPrice'));
   }.property('levelOneSelection', 'levelTwoSelection', 'displayedMinPrice', 'displayedMaxPrice'),
   availableItemsNoPriceLimits: function () {
     return getAvailableItems(this.get('levelTwoSelection'), this.get('levelTwoItems'), this.get('model'));
   }.property('levelOneSelection', 'levelTwoSelection'),
+  noItemsFound: function () {
+    if (this.get('productsToDisplay') === null) {
+      return false;
+    } else {
+      return (this.get('productsToDisplay').length === 0);
+    }
+  }.property('productsToDisplay'),
   actions: {
     levelOneClicked: function () {
       this.set('levelTwoDisplayed', false);
@@ -61,7 +69,7 @@ export default Ember.Controller.extend({
         return (item.id === selectionId);
       })[0];
       this.set('levelOneSelection', selectedItem);
-      this.set('levelTwoSelection', null);
+      this.set('levelTwoSelection', []);
       this.set('displayedMinPrice', this.get('actualMinMaxPrice')[0]);
       this.set('displayedMaxPrice', this.get('actualMinMaxPrice')[1]);
     },
@@ -69,7 +77,7 @@ export default Ember.Controller.extend({
       var selectedItem = this.get('model').filter(function (item) {
         return (item.id === selectionId);
       })[0];
-      this.set('levelTwoSelection', selectedItem);
+      this.set('levelTwoSelection', toggleSelectionInList(selectedItem, Ember.copy(this.get('levelTwoSelection'))));
       this.set('displayedMinPrice', this.get('actualMinMaxPrice')[0]);
       this.set('displayedMaxPrice', this.get('actualMinMaxPrice')[1]);
     },
@@ -85,14 +93,22 @@ export default Ember.Controller.extend({
       this.set('displayedMaxPrice', Math.floor(value[1]));
     }
   }
-
 });
 
-// In the interest of Functionl Programming, these method has no side effects ;)
+/**
+ * Gets the list of available items according to criteria given.
+ * In the interest of Functional Programming, there are no side effects.
+ * @param levelTwoSelection The user's selection of level two items.
+ * @param levelTwoItems All level two items.
+ * @param items All items.
+ * @param userMinPrice The user selected minimum price.
+ * @param userMaxPrice The user selected maximum price.
+ * @returns {*} All available items.
+ */
 function getAvailableItems(levelTwoSelection, levelTwoItems, items, userMinPrice, userMaxPrice) {
   var levelTwoCategoriesToInclude = [];
-  if (levelTwoSelection !== null) {
-    levelTwoCategoriesToInclude.push(levelTwoSelection);
+  if (levelTwoSelection.length > 0) {
+    levelTwoCategoriesToInclude = levelTwoSelection;
   } else {
     levelTwoCategoriesToInclude = levelTwoItems;
   }
@@ -113,6 +129,12 @@ function getAvailableItems(levelTwoSelection, levelTwoItems, items, userMinPrice
   });
 }
 
+/**
+ * Get the min and max price of all items in the list.
+ * In the interest of Functional Programming, there are no side effects.
+ * @param items An array of items.
+ * @returns {*} An array of format [min, max].
+ */
 function getMinMaxPrice(items) {
   if (items.length < 1) {
     return [0, 2000];
@@ -124,7 +146,6 @@ function getMinMaxPrice(items) {
       return (a.price > b.price ? 1 : -1);
     }
   })[0].price;
-
   var max = items.sort(function (a, b) {
     if (a.price === b.price) {
       return 0;
@@ -132,6 +153,27 @@ function getMinMaxPrice(items) {
       return (a.price < b.price ? 1 : -1);
     }
   })[0].price;
-
   return [min, max];
+}
+
+/**
+ * If the item is in the list, it will be removed. If it is not in the list, it will be added.
+ * In the interest of Functional Programming, there are no side effects.
+ * @param newItem Item to be added or removed.
+ * @param list  List of items.
+ * @returns {*} The modified list.
+ */
+function toggleSelectionInList(newItem,list) {
+  var idx = -1;
+  list.forEach(function(item, index) {
+    if (item.id === newItem.id) {
+      idx = index;
+    }
+  });
+  if (idx > -1) {
+    list.splice(idx, 1);
+  } else {
+    list.push(newItem);
+  }
+  return list;
 }
